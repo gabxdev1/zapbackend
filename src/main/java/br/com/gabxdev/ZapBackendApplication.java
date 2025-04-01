3091b36fcf4f77acc4dd22e7fedd0ac1f1e25dd2
gabxdev1 <gabriel.duraes@alunos.unis.edu.br>
2025-04-01 12:49:04 -0300

    first commit

---
+++ src/main/java/br/com/gabxdev/ZapBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabxdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabxdev/ZapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabxdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
